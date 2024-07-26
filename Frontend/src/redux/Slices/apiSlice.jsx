0d12900327fa975a24fb02d484a988866e4b649e
Mukund Agrawal <148519456+MukundAgrawal02@users.noreply.github.com>
2024-07-26 13:42:05 +0530

    added deployed api url

--- Frontend/src/redux/Slices/apiSlice.jsx
+++ Frontend/src/redux/Slices/apiSlice.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   // Define your base URL here
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://sss-practice.vercel.app/' }),
   // Define your endpoints here
   endpoints: (builder) => ({
     getAnimals: builder.query({
