e5366cde4464d1b3b9640fa805692b9fbcfd2f4a
Mukund Agrawal <148519456+MukundAgrawal02@users.noreply.github.com>
2024-07-26 13:40:31 +0530

    update in api

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-     proxy:{
-       '/api': {
-         target: 'https://sss-practice.vercel.app', 
-       }
-     }
-    },
   plugins: [react()],
 })
